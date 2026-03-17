4523f9cd52a2a2268497f41a598aaaab2a194498
EC2 Default User <ec2-user@ip-172-31-42-113.ap-northeast-2.compute.internal>
2026-03-17 00:16:16 +0000

    feat:sprint10 public complete

---
+++ ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apps : [{
+  name: "pandamarket", 
+  script: 'build/main.js',
+  instances : 0	  
+  }]		
+};
---
+++ infra/ec2/start.sh
@@ -0,0 +1,15 @@
+npm install pm2@latest -g
+
+pm2
+
+cat package.json
+
+npm run build
+
+pm2 start build/main.js
+
+pm2 logs
+
+pm2 list
+
+
